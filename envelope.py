7c2250da82ddc61e211a87cb6d68a9a51d7d3a5c
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2014-05-23 00:03:07 +0900

    Arrows added, equal aspect ratio, etc

--- envelope.py
+++ envelope.py
@@ -4,15 +4,15 @@ from mpl_toolkits.axes_grid.axislines import SubplotZero
 import numpy as np
 
 # x = np.linspace(-x_range, x_range, x_steps)
-x_range = 50
+x_range = 10
 x_steps = 200
 
 # param in range(-param_range, param_range + 1, param_increment)
-param_range, param_increment = 18, 3
-#param_range, param_increment = 24, 2
+param_range, param_increment = 5, 2
+#param_range, param_increment = 14, 1
 
 # To compute a suitable range of the function
-param_bound = 16
+param_bound = 5
 
 # Function to be drawn parameterized by t
 def func(x, t=1):
@@ -20,9 +20,10 @@ def func(x, t=1):
 
 # Lower and upper bounds of the graph
 y_min = func(x=0, t=param_bound)
-y_max = x_range ** 2 / 4 + 100
+#y_max = x_range ** 2 / 4 + 100
+y_max = x_range ** 2 / 4
 
-def subplots():
+def subplots(x_label='$x$', y_label='$y$'):
     "Custom subplots with axes through the origin"
     #fig, ax = plt.subplots()
     #fig = plt.figure(1)
@@ -30,25 +31,22 @@ def subplots():
     ax = SubplotZero(fig, 111)
     fig.add_subplot(ax)
     
-    #ax.axhline(linewidth=1.7, color="k")
-    #ax.axvline(linewidth=1.7, color="k")
-    
-    plt.xticks([])
-    plt.yticks([])
-    
-    # from http://stackoverflow.com/questions/17646247/how-to-make-fuller-axis-arrows-with-matplotlib
-    #ax.text(1.01, 0, '$x$', transform=BlendedGenericTransform(ax.transAxes, ax.transData), va='center')
-    #ax.text(0, 1.02, '$y$', transform=BlendedGenericTransform(ax.transData, ax.transAxes), ha='center')
-    ax.text(1.05, 0, '$x$', transform=BlendedGenericTransform(ax.transAxes, ax.transData), va='center')
-    ax.text(0, 1.06, '$y$', transform=BlendedGenericTransform(ax.transData, ax.transAxes), ha='center')
-    
     for direction in ["xzero", "yzero"]:
-        ax.axis[direction].set_axisline_style("-|>")
+        #ax.axis[direction].set_axisline_style("-|>")
         ax.axis[direction].set_visible(True)
     
     for direction in ["left", "right", "bottom", "top"]:
         ax.axis[direction].set_visible(False)
+ 
+    #plt.xticks([])
+    #plt.yticks([])
     
+    # from http://stackoverflow.com/questions/17646247/how-to-make-fuller-axis-arrows-with-matplotlib
+    #ax.text(1.01, 0, '$x$', transform=BlendedGenericTransform(ax.transAxes, ax.transData), va='center')
+    #ax.text(0, 1.02, '$y$', transform=BlendedGenericTransform(ax.transData, ax.transAxes), ha='center')
+    ax.text(1.06, 0, x_label, transform=BlendedGenericTransform(ax.transAxes, ax.transData), va='center')
+    ax.text(0, 1.06, y_label, transform=BlendedGenericTransform(ax.transData, ax.transAxes), ha='center')
+        
     return fig, ax
 
 fig, ax = subplots()  # Call the local version, not plt.subplots()
@@ -58,9 +56,17 @@ for t in range(-param_range, param_range + 1, param_increment):
     y = func(x, t)
     ax.plot(x, y, 'k-')
 
+ax.set_aspect('equal')
+
 #plt.ylim(ymin=y_min)
 #plt.ylim([y_min, y_max])
 ax.set_ylim(y_min, y_max)
+
+# from http://stackoverflow.com/questions/17646247/how-to-make-fuller-axis-arrows-with-matplotlib
+#plt.arrow(x_range, -0.003, 0.1, 0, width=0.015, color="k", clip_on=False, head_width=30, head_length=2)
+#plt.arrow(0.003, y_max, 0, 0.1, width=0.015, color="k", clip_on=False, head_width=2, head_length=30)
+plt.arrow(x_range, 0, 1, 0, color="k", clip_on=False, head_width=0.12, head_length=0.12)
+plt.arrow(0.003, y_max, 0, 0.1, color="k", clip_on=False, head_width=0.12, head_length=0.12)
 
 plt.show()
 #plt.savefig('envelope1.svg', transparent=True, bbox_inches='tight', pad_inches=0)
