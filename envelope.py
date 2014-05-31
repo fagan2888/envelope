807eb43aeb351c046f555fa826f6160ee78cd396
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2014-05-31 11:48:48 +0900

    Minor changes

--- envelope.py
+++ envelope.py
@@ -21,13 +21,13 @@ def func(x, t):
     return t*x - t**2
 
 
-# param_array = np.linspace(-param_max, param_max, num_params)
-paramdicts = [
-    {'param_max': 2, 'num_params': 13},
-    {'param_max': 3, 'num_params': 31},
+# pvalue_array = np.linspace(-pvalue_max, pvalue_max, num_pvalues)
+pvalue_dicts = [
+    {'pvalue_max': 2, 'num_pvalues': 13},
+    {'pvalue_max': 3, 'num_pvalues': 31},
     ]
-#param_max, param_grids_per_unit = 2, 3
-#param_max, param_grids_per_unit = 3, 5
+#pvalue_max, pvalue_grids_per_unit = 2, 3
+#pvalue_max, pvalue_grids_per_unit = 3, 5
 
 
 # x = np.array([x_min, x_max])
@@ -43,7 +43,7 @@ y_max = x_max**2 / 4 + 1
 if MULT_SUBPLOTS:
     WSPACE = 0.5
     plt.rcParams['figure.figsize'] = \
-        2.4 * len(paramdicts) + WSPACE * (len(paramdicts) - 1), 2.4
+        2.4 * len(pvalue_dicts) + WSPACE * (len(pvalue_dicts) - 1), 2.4
 else:
     plt.rcParams['figure.figsize'] = 2.4, 2.4  # inches: default 8, 6
 plt.rcParams['lines.linewidth'] = 0.5
@@ -81,9 +81,9 @@ def customize_axes(ax, x_label_pos=[1, 0], y_label_pos=[0, 1],
 x = np.array([x_min, x_max])
 
 
-def do_plot(ax, param_max, num_params):
-    param_array = np.linspace(-param_max, param_max, num_params)
-    for t in param_array:
+def do_plot(ax, pvalue_max, num_pvalues):
+    pvalue_array = np.linspace(-pvalue_max, pvalue_max, num_pvalues)
+    for t in pvalue_array:
         y = func(x, t)
         ax.plot(x, y, 'k-')
 
@@ -97,12 +97,12 @@ if MULT_SUBPLOTS:
     fig.subplots_adjust(wspace=WSPACE)  # http://matplotlib.org/faq/howto_faq.html
 
     axes = []
-    for fignum, paramdict in enumerate(paramdicts):
+    for fignum, pvalue_dict in enumerate(pvalue_dicts):
         # SubplotZero(fig, numRows, numCols, plotNum)
-        axes.append(SubplotZero(fig, 1, len(paramdicts), fignum+1))
+        axes.append(SubplotZero(fig, 1, len(pvalue_dicts), fignum+1))
         fig.add_subplot(axes[fignum])
         axes[fignum] = customize_axes(axes[fignum], x_label_Pos, y_label_Pos)
-        do_plot(axes[fignum], paramdict['param_max'], paramdict['num_params'])
+        do_plot(axes[fignum], pvalue_dict['pvalue_max'], pvalue_dict['num_pvalues'])
 
     if SAVEFILE:
         TRANS = (FILEFORMAT.lower() in ['png', 'svc'])
@@ -112,13 +112,13 @@ if MULT_SUBPLOTS:
 
 elif SAVEFILE:
     TRANS = (FILEFORMAT.lower() in ['png', 'svc'])
-    for fignum, paramdict in enumerate(paramdicts):
+    for fignum, pvalue_dict in enumerate(pvalue_dicts):
         fig = plt.figure(1)
         ax = SubplotZero(fig, 111)
         fig.add_subplot(ax)
         ax = customize_axes(ax, x_label_Pos, y_label_Pos)
 
-        do_plot(ax, paramdict['param_max'], paramdict['num_params'])
+        do_plot(ax, pvalue_dict['pvalue_max'], pvalue_dict['num_pvalues'])
         plt.savefig(FILNENAME_BASE + str(fignum) + '.' + FILEFORMAT.lower(),
                     transparent=TRANS, bbox_inches='tight', pad_inches=0)
 
@@ -128,5 +128,5 @@ else:
     fig.add_subplot(ax)
     ax = customize_axes(ax, x_label_Pos, y_label_Pos)
 
-    do_plot(ax, paramdicts[FIGNUM]['param_max'], paramdicts[FIGNUM]['num_params'])
+    do_plot(ax, pvalue_dicts[FIGNUM]['pvalue_max'], pvalue_dicts[FIGNUM]['num_pvalues'])
     plt.show()
